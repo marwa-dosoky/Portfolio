0fbccd4bef098fcf517ec5ee09942e656cee2206
marwa <marwa@HMLaptop>
2021-07-17 18:10:21 +0400

    make the navigation dynamic form the page sections

--- UdacityProject/README.md
+++ UdacityProject/README.md
@@ -2,8 +2,21 @@
 
 ## Table of Contents
 
-* [Note](#Note)
+* [Project Name](#Project Name)
+* [Author](#Author)
+* [Technologies Used](#Technologies Used)
+* [Project Discription ](#Project Discription )
 
-## Note
+##Project Name
+Marwa Dosoky CV
 
+##Technologies Used
+HTML
+CSS
+JavaScript
+
+##Author
+Updated by Marwa Dosoky
+
+## Project Discription 
 Hi, This project is done by Marwa Dosoky, as an assignment in the web development track
--- UdacityProject/js/app.js
+++ UdacityProject/js/app.js
@@ -1,26 +1,25 @@
-//// Build the navbar
-var navbar_items = [ [ 'bio_link', 'bio', 'Bio' ],
-		[ 'techSkills_link', 'techSkills', 'Technical Skills' ],
-		[ 'education_link', 'education', 'Education' ],
-		[ 'activities_link', 'activities', 'Activities' ] ];
 
+//// Build the navbar
+// list of sections
+let sections;
+// list of navigation links
+let navbarLinks;
+
+// build the navigation menu
 function builtNavBar() {
-	var navBarHtml = "";
-	navbar_items.forEach(function(item, index, array) {
-		navBarHtml += "<li><a id=\"" + item[0] + "\" href=\"#" + item[1]
-				+ "\" class=\"menu__link\">" + item[2] + "</a></li>";
+
+	sections = document.querySelectorAll('section');
+	let navBarHtml = "";
+	sections.forEach(function(item, index, array) {
+
+		navBarHtml += "<li><a id=\"" + item.dataset.nav + "Link"
+				+ "\"  onclick=\"gototSection(" + item.getAttribute('id')
+				+ ")\" " + " class=\"menu__link\">" + item.dataset.nav
+				+ "</a></li>";
 	});
+
 	document.getElementById('navbar__list').innerHTML = navBarHtml;
-	
-//lazy initialization for the page elements
-	 bio = document.getElementById('bio');
-	 techSkills = document.getElementById('techSkills');
-	 education = document.getElementById('education');
-	 activities = document.getElementById('activities');
-	 bio_link = document.getElementById('bio_link');
-	 techSkills_link = document.getElementById('techSkills_link');
-	 education_link = document.getElementById('education_link');
-	 activities_link = document.getElementById('activities_link');
+
 }
 
 // check if the section is in the viewport
@@ -35,45 +34,54 @@ function isInViewport(el) {
 }
 //
 
+// clear the active state for the viewed section to only highlight the
+// viewed
 function reset_highlight_class() {
+
 	// clear the active state class fot the navbar to only highlight the viewed
-	bio_link.classList.remove("link_active");
-	techSkills_link.classList.remove("link_active");
-	education_link.classList.remove("link_active");
-	activities_link.classList.remove("link_active");
-	// clear the active state for the viewed section to only highlight the
-	// viewed
-	// your-active-class
-	bio.classList.remove("your-active-class");
-	techSkills.classList.remove("your-active-class");
-	education.classList.remove("your-active-class");
-	activities.classList.remove("your-active-class");
+	sections.forEach(function(item, index, array) {
+		// clear the active state from the section
+		item.classList.remove("your-active-class");
+
+		// clear the active state from the navigation link
+		// let navbarLink = document.getElementById(item.dataset.nav + "Link");
+		// navbarLink.remove("link_active");
+
+	});
 
 }
 // add listener to highlight the active section and the navigation link
 document.addEventListener('scroll', function() {
 
-	if (isInViewport(bio)) {
-		reset_highlight_class();
-		bio_link.classList.add("link_active");
-		bio.classList.add("your-active-class");
-	}
-	if (isInViewport(techSkills)) {
-		reset_highlight_class();
-		techSkills_link.classList.add("link_active");
-		techSkills.classList.add("your-active-class");
-	}
-	if (isInViewport(education)) {
-		reset_highlight_class();
-		education_link.classList.add("link_active");
-		education.classList.add("your-active-class");
-	}
-	if (isInViewport(activities)) {
-		reset_highlight_class();
-		activities_link.classList.add("link_active");
-		activities.classList.add("your-active-class");
-	}
+	sections.forEach(function(item, index, array) {
+
+		if (isInViewport(item)) {
+			reset_highlight_class();
+
+			item.classList.add("your-active-class");
+
+			// let navbarLink = document.getElementById(item.dataset.nav
+			// + "Link");
+			// console.log(navbarLink);
+			// navbarLink.classList.add("link_active");
+		}
+	});
 
 }, {
 	passive : true
 });
+
+// go to section given the section ID
+// NEED HELP :I am sending the sectionID, i don't know why all the section DOM
+// element is sent
+function gototSection(sectionID) {
+
+	// let element = document.getElementById(sectionID);
+	// element.scrollIntoView({behavior: "smooth", block: "end", inline:
+	// "nearest"});
+
+	sectionID.scrollIntoView({
+		behavior : "smooth"
+	});
+
+}
